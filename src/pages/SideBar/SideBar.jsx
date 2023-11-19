8d7437fc9a27d80e269a6fd5f2a9c424f68e2336
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2023-11-19 02:35:49 +0200

    add favorites new redux logic

---
+++ src/pages/SideBar/SideBar.jsx
@@ -0,0 +1,52 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+
+import Button from 'components/Button';
+
+import * as s from './SideBar.styled';
+
+const SideBarRoot = document.getElementById('sideBar-root');
+
+export const SideBar = ({ onClose }) => {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
+  const handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
+
+  return createPortal(
+    <s.Backdrop onClick={handleBackdropClick}>
+      <s.Field>
+        <s.CloseButton onClick={onClose} aria-label="Close">
+          <s.IconBtn />
+        </s.CloseButton>
+        <s.Title>Rental</s.Title>
+        <s.Nav>
+          <s.StyledLink to="/" onClick={onClose}>
+            <Button> Home </Button>
+          </s.StyledLink>
+          <s.StyledLink to="/catalog" onClick={onClose}>
+            <Button> Catalog </Button>
+          </s.StyledLink>
+          <s.StyledLink to="/favorites" onClick={onClose}>
+            <Button> Favorites </Button>
+          </s.StyledLink>
+        </s.Nav>
+      </s.Field>
+    </s.Backdrop>,
+    SideBarRoot
+  );
+};
---
+++ src/pages/SideBar/SideBar.styled.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import { ReactComponent as CloseIconBtn } from '../../icons/Catalog/x-close.svg';
+
+export const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+
+  //   display: flex;
+  //   justify-content: center;
+  //   align-items: center;
+
+  background-color: rgba(18, 20, 23, 0.5);
+`;
+
+export const Field = styled.aside`
+  position: absolute;
+
+  min-width: 400px;
+  height: 100vh;
+
+  padding: 100px 150px;
+  box-sizing: border-box;
+
+  border-top-right-radius: 24px;
+  border-bottom-right-radius: 24px;
+
+  background-color: #fff;
+`;
+
+export const CloseButton = styled.button`
+  position: absolute;
+  top: 16px;
+  right: 16px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 0;
+
+  cursor: pointer;
+  border: none;
+
+  background-color: transparent;
+`;
+
+export const IconBtn = styled(CloseIconBtn)`
+  width: 100%;
+  height: 100%;
+
+  color: black;
+`;
+
+export const Title = styled.h2`
+  text-transform: uppercase;
+  color: black;
+  font-size: 40px;
+  font-weight: bold;
+
+  margin-bottom: 150px;
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+  //   padding-top: 30px;
+`;
+
+export const StyledLink = styled(NavLink)`
+  text-decoration: none;
+
+  &:hover,
+  &:focus,
+  &.active {
+    background-color: red;
+    box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+      0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+  }
+`;
